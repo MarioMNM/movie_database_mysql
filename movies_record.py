c73b6f6c076cef39ad7b37f22ebf100f56ba909d
Mario Novella <mario.novella@gmail.com>
2022-11-16 17:35:09 +0100

    finished MovieFrame

--- movies_record.py
+++ movies_record.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import pymysql
 from tkinter import ttk
+from tkcalendar import DateEntry
 import random
 import tkinter.messagebox
 import datetime
@@ -37,6 +38,9 @@ class MovieRecords:
         self.Starring = StringVar()
         self.Genres = StringVar()
 
+        self.Prod = StringVar()
+        self.Music = StringVar()
+
         self.AudioLanguages = StringVar()
         self.Subtitles = StringVar()
         self.Writters = StringVar()
@@ -60,66 +64,14 @@ class MovieRecords:
         TopFrame3 = Frame(MainFrame, bd=10, width=1340, height=500, relief=RIDGE)
         TopFrame3.grid(padx=5, pady=10)
 
-        RightFrame1 = Frame(TopFrame3, bd=5, width=320, height=400, padx=2, bg='cadetblue', relief=RIDGE)
-        RightFrame1.pack(side=RIGHT, pady=1)
+        BottomFrame = Frame(MainFrame, bd=10, width=1340, height=120, padx=2, relief=RIDGE)
+        BottomFrame.grid(padx=5, pady=10)
 
-        InnerRightFrame = Frame(RightFrame1, bd=5, width=310, height=300, padx=2, relief=RIDGE)
-        InnerRightFrame.pack(side=TOP, pady=2)
+        TopFrame = Frame(TopFrame3, bd=5, width=320, height=400, padx=2, bg='cadetblue', relief=RIDGE)
+        TopFrame.pack(pady=1)
 
-        CalendarFrame = Frame(InnerRightFrame, bd=5, width=310, height=300, padx=2, relief=RIDGE)
-        CalendarFrame.pack(side=TOP, pady=1)
-
-        UnitsFrame = Frame(InnerRightFrame, bd=5, width=310, height=300, padx=2, relief=RIDGE)
-        UnitsFrame.pack(side=TOP, pady=1)
-
-        ResultFrame = Frame(InnerRightFrame, bd=5, width=330, height=50, padx=2, relief=RIDGE)
-        ResultFrame.pack(side=TOP, pady=1)
-
-        ResultFrameLeft = Frame(ResultFrame, bd=0, width=130, height=50, padx=2, relief=RIDGE)
-        ResultFrameLeft.grid(row=0, column=0, pady=1)
-
-        ResultFrameRight = Frame(ResultFrame, bd=0, width=200, height=50, padx=2, relief=RIDGE)
-        ResultFrameRight.grid(row=0, column=1)
-
-        UnitNo = Frame(UnitsFrame, bd=0, width=50, height=300, padx=2, relief=RIDGE)
-        UnitNo.grid(row=0, column=0, pady=2)
-        UnitSubject = Frame(UnitsFrame, bd=1, width=210, height=300, padx=2, pady=4, relief=RIDGE)
-        UnitSubject.grid(row=0, column=1, pady=2)
-        UnitScore = Frame(UnitsFrame, bd=0, width=50, height=300, padx=2, relief=RIDGE)
-        UnitScore.grid(row=0, column=2, pady=1)
-        # ---------------------------------------------------------------------------------------------
-        LeftFrame = Frame(TopFrame3, bd=5, width=1340, height=400, padx=2, bg='cadetblue', relief=RIDGE)
-        LeftFrame.pack(side=RIGHT, pady=0)
-        CourseFrame = Frame(LeftFrame, bd=5, width=600, height=180, padx=4, relief=RIDGE)
-        CourseFrame.pack(side=TOP, pady=2)
-
-        LeftFrame2 = Frame(LeftFrame, bd=5, width=600, height=180, padx=2, bg='cadetblue', relief=RIDGE)
-        LeftFrame2.pack(side=TOP)
-        MovieStatusFrame = Frame(LeftFrame2, bd=5, width=300, height=170, padx=2, relief=RIDGE)
-        MovieStatusFrame.pack(side=LEFT)
-        DegreeFrame = Frame(LeftFrame2, bd=5, width=300, height=170, padx=2, relief=RIDGE)
-        DegreeFrame.pack(side=RIGHT)
-
-        ButtonFrame = Frame(LeftFrame, bd=5, width=320, height=50, padx=2, relief=RIDGE)
-        ButtonFrame.pack(side=LEFT, pady=3)
-
-        LeftFrame2 = Frame(TopFrame3, bd=5, width=320, height=50, padx=2,  bg='cadetblue', relief=RIDGE)
-        LeftFrame2.pack(side=LEFT, pady=5)
-        MovieFrame = Frame(LeftFrame2, bd=5, width=280, height=50, padx=2, relief=RIDGE)
+        MovieFrame = Frame(TopFrame, bd=5, width=280, height=50, padx=2, relief=RIDGE)
         MovieFrame.pack(side=TOP, pady=3)
-        LanguageFrame = Frame(LeftFrame2, bd=5, width=280, height=50, padx=3, relief=RIDGE)
-        LanguageFrame.pack(side=TOP)
-
-        TopFrame11 = Frame(Tab2Frame, bd=10, width=1340, height=60, relief=RIDGE)
-        TopFrame11.grid(row=0, column=0)
-        TopFrame12 = Frame(Tab2Frame, bd=10, width=280, height=100, relief=RIDGE)
-        TopFrame12.grid(row=1, column=0)
-
-        TopFrame12a = Frame(TopFrame12, bd=10, width=1000, height=100, relief=RIDGE)
-        TopFrame12a.grid(row=1, column=1)
-
-        TopFrame12b = Frame(TopFrame12, bd=10, width=340, height=100, relief=RIDGE)
-        TopFrame12b.grid(row=1, column=0)
         # ---------------------------------------------------------------------------------------------
         self.lblMovieID = Label(MovieFrame, font=('arial', 12, 'bold'), text='Movie ID', bd=7, anchor='w', justify=LEFT)
         self.lblMovieID.grid(row=0, column=0, sticky=W, padx=5, pady=5)
@@ -140,53 +92,134 @@ class MovieRecords:
                                   textvariable=self.Directors)
         self.txtDirectors.grid(row=2, column=1)
 
-        self.lblDescription = Label(MovieFrame, font=('arial', 12, 'bold'), text='Description', bd=7, anchor='w',
-                                  justify=LEFT)
-        self.lblDescription.grid(row=3, column=0, sticky=W, padx=5, pady=5)
-        self.txtDescription = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
-                                textvariable=self.Description)
-        self.txtDescription.grid(row=3, column=1)
-
-        self.lblStarring = Label(MovieFrame, font=('arial', 12, 'bold'), text='Starring', bd=7, anchor='w',
-                                    justify=LEFT)
-        self.lblStarring.grid(row=4, column=0, sticky=W, padx=5, pady=5)
-        self.txtStarring = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
-                                    textvariable=self.Starring)
-        self.txtStarring.grid(row=4, column=1)
-
         self.lblGenres = Label(MovieFrame, font=('arial', 12, 'bold'), text='Genres', bd=7, anchor='w',
-                                 justify=LEFT)
-        self.lblGenres.grid(row=5, column=0, sticky=W, padx=5, pady=5)
+                               justify=LEFT)
+        self.lblGenres.grid(row=3, column=0, sticky=W, padx=5, pady=5)
         self.cboGenres = ttk.Combobox(MovieFrame, textvariable=self.Genres, width=21,
                                       font=('arial', 12, 'bold'), state='readonly')
         self.cboGenres['value'] = ('', 'Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary',
                                    'Drama', 'Fantasy', 'History', 'Horror', 'Mystery', 'Romance', 'Science Fiction',
                                    'Thriller', 'War', 'Western')
         self.cboGenres.current(0)
-        self.cboGenres.grid(row=5, column=1)
+        self.cboGenres.grid(row=3, column=1)
         # ---------------------------------------------------------------------------------------------
-        self.lblAudio = Label(LanguageFrame, font=('arial', 12, 'bold'), text='Audio languages', bd=7, anchor='w',
-                              justify=LEFT)
-        self.lblAudio.grid(row=0, column=0, sticky=W, padx=5, pady=5)
-        self.cboAudio = ttk.Combobox(LanguageFrame, textvariable=self.AudioLanguages, width=21,
-                                      font=('arial', 12, 'bold'), state='readonly')
-        self.cboAudio['value'] = ('', 'English', 'Español (España)', 'Italiano', 'Español (Latinoamérica)', 'Polski',
-                                  'Čeština', '日本語', 'Português', 'Türkçe', 'Français')
-        self.cboAudio.current(0)
-        self.cboAudio.grid(row=0, column=1)
+        self.lblReleaseDate = Label(MovieFrame, font=('arial', 12, 'bold'), text='Release Date', bd=7, anchor='w',
+                                    justify=LEFT)
+        self.lblReleaseDate.grid(row=0, column=2, sticky=W, padx=5, pady=5)
+        self.calReleaseDate = DateEntry(MovieFrame, selectmode='day', date_pattern='dd/MM/yyyy',
+                                        textvariable=self.ReleaseDate)
+        self.calReleaseDate.grid(row=0, column=3, sticky=W, padx=5, pady=5)
 
-        self.lblSubtitles = Label(LanguageFrame, font=('arial', 12, 'bold'), text='Subtitles', bd=7, anchor='w',
+        self.lblProd = Label(MovieFrame, font=('arial', 12, 'bold'), text='Produced by', bd=7, anchor='w', justify=LEFT)
+        self.lblProd.grid(row=1, column=2, sticky=W, padx=5, pady=5)
+        self.txtProd = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left',
+                                width=23, textvariable=self.Prod)
+        self.txtProd.grid(row=1, column=3)
+
+        self.lblWritter = Label(MovieFrame, font=('arial', 12, 'bold'), text='Writters', bd=7, anchor='w', justify=LEFT)
+        self.lblWritter.grid(row=2, column=2, sticky=W, padx=5, pady=5)
+        self.txtWritter = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left',
+                             width=23, textvariable=self.Writters)
+        self.txtWritter.grid(row=2, column=3)
+
+        self.lblMusic = Label(MovieFrame, font=('arial', 12, 'bold'), text='Music by', bd=7, anchor='w', justify=LEFT)
+        self.lblMusic.grid(row=3, column=2, sticky=W, padx=5, pady=5)
+        self.txtMusic = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left',
+                             width=23, textvariable=self.Music)
+        self.txtMusic.grid(row=3, column=3)
+        # ---------------------------------------------------------------------------------------------
+        def my_upd(*args):
+            time = str(hr.get()) + ":" + str(mn.get()) + ":" + str(sc.get())
+            l1.config(text=time)
+
+        self.lblDuration = Label(MovieFrame, font=('arial', 12, 'bold'), text='Duration')
+        self.lblDuration.grid(row=0, column=4, sticky=W, padx=5, pady=5)
+        l1 = Label(MovieFrame, font=('arial', 12, 'bold'))  # show date
+        l1.grid(row=0, column=5, sticky=W, padx=5, pady=5)
+        l_hr = Label(MovieFrame, text='Hour')
+        l_hr.grid(row=1, column=4, sticky=W, padx=5, pady=5)
+        hr = Scale(MovieFrame, from_=0, to=5,
+                   orient='horizontal', length=150, command=my_upd)
+        hr.grid(row=1, column=5)
+        l_mn = Label(MovieFrame, text='Mintue')
+        l_mn.grid(row=2, column=4, sticky=W, padx=5, pady=5)
+        mn = Scale(MovieFrame, from_=0, to=59,
+                   orient='horizontal', length=150, command=my_upd)
+        mn.grid(row=2, column=5)
+        l_sc = Label(MovieFrame, text='Second')
+        l_sc.grid(row=3, column=4, sticky=W, padx=5, pady=5)
+        sc = Scale(MovieFrame, from_=0, to=59,
+                   orient='horizontal', length=150, command=my_upd)
+        sc.grid(row=3, column=5)
+        self.Duration.trace('w', my_upd)
+        my_upd()
+        # ---------------------------------------------------------------------------------------------
+        self.lblAreSubtitles = Label(MovieFrame, font=('arial', 12, 'bold'), text='Subtitles Available', bd=7,
+                                     anchor='w',
+                                     justify=LEFT)
+        self.lblAreSubtitles.grid(row=0, column=6, sticky=W, padx=5, pady=5)
+        self.cboAreSubtitles = ttk.Combobox(MovieFrame, textvariable=self.AreSubtitles, width=21,
+                                            font=('arial', 12, 'bold'), state='readonly')
+        self.cboAreSubtitles['value'] = ('', 'Yes', 'No')
+        self.cboAreSubtitles.current(0)
+        self.cboAreSubtitles.grid(row=0, column=7)
+
+        self.lblHDR = Label(MovieFrame, font=('arial', 12, 'bold'), text='HDR available', bd=7,
+                            anchor='w',
+                            justify=LEFT)
+        self.lblHDR.grid(row=1, column=6, sticky=W, padx=5, pady=5)
+        self.cboHDR = ttk.Combobox(MovieFrame, textvariable=self.HDR, width=21,
+                                   font=('arial', 12, 'bold'), state='readonly')
+        self.cboHDR['value'] = ('', 'Yes', 'No')
+        self.cboHDR.current(0)
+        self.cboHDR.grid(row=1, column=7)
+
+        self.lblUHD = Label(MovieFrame, font=('arial', 12, 'bold'), text='UHD available', bd=7,
+                            anchor='w',
+                            justify=LEFT)
+        self.lblUHD.grid(row=2, column=6, sticky=W, padx=5, pady=5)
+        self.cboUHD = ttk.Combobox(MovieFrame, textvariable=self.UHD, width=21,
+                                   font=('arial', 12, 'bold'), state='readonly')
+        self.cboUHD['value'] = ('', 'Yes', 'No')
+        self.cboUHD.current(0)
+        self.cboUHD.grid(row=2, column=7)
+
+        self.lblPG = Label(MovieFrame, font=('arial', 12, 'bold'), text='Movie Age Rating', bd=7, anchor='w',
+                           justify=LEFT)
+        self.lblPG.grid(row=3, column=6, sticky=W, padx=5, pady=5)
+        self.cboPG = ttk.Combobox(MovieFrame, textvariable=self.PG, width=21,
+                                  font=('arial', 12, 'bold'), state='readonly')
+        self.cboPG['value'] = ('', 'G', 'PG', 'PG-13', 'R', 'NC-17')
+        self.cboPG.current(0)
+        self.cboPG.grid(row=3, column=7)
+        # ---------------------------------------------------------------------------------------------
+        self.lblDescription = Label(MovieFrame, font=('arial', 12, 'bold'), text='Description', bd=7, anchor='w',
+                                    justify=LEFT)
+        self.lblDescription.grid(row=4, column=0, sticky=W, padx=5, pady=5)
+        self.txtDescription = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
+                                    textvariable=self.Description)
+        self.txtDescription.grid(row=4, column=1, columnspan=7, ipadx=500)
+
+        self.lblStarring = Label(MovieFrame, font=('arial', 12, 'bold'), text='Starring', bd=7, anchor='w',
+                                    justify=LEFT)
+        self.lblStarring.grid(row=5, column=0, sticky=W, padx=5, pady=5)
+        self.txtStarring = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
+                                    textvariable=self.Starring)
+        self.txtStarring.grid(row=5, column=1, columnspan=7, ipadx=500)
+
+        self.lblAudio = Label(MovieFrame, font=('arial', 12, 'bold'), text='Audio languages', bd=7, anchor='w',
+                              justify=LEFT)
+        self.lblAudio.grid(row=6, column=0, sticky=W, padx=5, pady=5)
+        self.txtAudio = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
+                              textvariable=self.AudioLanguages)
+        self.txtAudio.grid(row=6, column=1, columnspan=7, ipadx=500)
+
+        self.lblSubtitles = Label(MovieFrame, font=('arial', 12, 'bold'), text='Subtitles', bd=7, anchor='w',
                                   justify=LEFT)
-        self.lblSubtitles.grid(row=1, column=0, sticky=W, padx=5, pady=5)
-        self.cboSubtitles = ttk.Combobox(LanguageFrame, textvariable=self.Subtitles, width=21,
-                                     font=('arial', 12, 'bold'), state='readonly')
-        if self.AreSubtitles == 'No':
-            self.cboSubtitles['value'] = ('')
-        else:
-            self.cboSubtitles['value'] = ('', 'English', 'Español (España)', 'Italiano', 'Español (Latinoamérica)', 'Polski',
-                                      'Čeština', '日本語', 'Português', 'Türkçe', 'Français')
-        self.cboSubtitles.current(0)
-        self.cboSubtitles.grid(row=1, column=1)
+        self.lblSubtitles.grid(row=7, column=0, sticky=W, padx=5, pady=5)
+        self.txtSubtitles = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
+                                  textvariable=self.Subtitles)
+        self.txtSubtitles.grid(row=7, column=1, columnspan=7, ipadx=500)
 
 
 if __name__ == '__main__':
