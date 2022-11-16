884d3bb4ad223c7f94fa5f052dda0ad8bdf578a6
Mario Novella <mario.novella@gmail.com>
2022-11-16 17:34:14 +0100

    finished MovieFrame

--- movies_record.py
+++ movies_record.py
@@ -103,12 +103,12 @@ class MovieRecords:
         ButtonFrame = Frame(LeftFrame, bd=5, width=320, height=50, padx=2, relief=RIDGE)
         ButtonFrame.pack(side=LEFT, pady=3)
 
-        RightFrame2 = Frame(TopFrame3, bd=5, width=320, height=50, padx=2,  bg='cadetblue', relief=RIDGE)
-        RightFrame2.pack(side=LEFT, pady=5)
-        MovieFrame = Frame(RightFrame2, bd=5, width=280, height=50, padx=2, relief=RIDGE)
+        LeftFrame2 = Frame(TopFrame3, bd=5, width=320, height=50, padx=2,  bg='cadetblue', relief=RIDGE)
+        LeftFrame2.pack(side=LEFT, pady=5)
+        MovieFrame = Frame(LeftFrame2, bd=5, width=280, height=50, padx=2, relief=RIDGE)
         MovieFrame.pack(side=TOP, pady=3)
-        ParentFrame = Frame(RightFrame2, bd=5, width=280, height=50, padx=3, relief=RIDGE)
-        ParentFrame.pack(side=TOP)
+        LanguageFrame = Frame(LeftFrame2, bd=5, width=280, height=50, padx=3, relief=RIDGE)
+        LanguageFrame.pack(side=TOP)
 
         TopFrame11 = Frame(Tab2Frame, bd=10, width=1340, height=60, relief=RIDGE)
         TopFrame11.grid(row=0, column=0)
@@ -123,8 +123,70 @@ class MovieRecords:
         # ---------------------------------------------------------------------------------------------
         self.lblMovieID = Label(MovieFrame, font=('arial', 12, 'bold'), text='Movie ID', bd=7, anchor='w', justify=LEFT)
         self.lblMovieID.grid(row=0, column=0, sticky=W, padx=5, pady=5)
-        self.txtMovieID = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', textvariable=self.MovieID)
+        self.txtMovieID = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left',
+                                width=23, textvariable=self.MovieID)
         self.txtMovieID.grid(row=0, column=1)
+
+        self.lblName = Label(MovieFrame, font=('arial', 12, 'bold'), text='Name', bd=7, anchor='w', justify=LEFT)
+        self.lblName.grid(row=1, column=0, sticky=W, padx=5, pady=5)
+        self.txtName = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
+                             textvariable=self.Name)
+        self.txtName.grid(row=1, column=1)
+
+        self.lblDirectors = Label(MovieFrame, font=('arial', 12, 'bold'), text='Directors', bd=7, anchor='w',
+                                  justify=LEFT)
+        self.lblDirectors.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+        self.txtDirectors = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
+                                  textvariable=self.Directors)
+        self.txtDirectors.grid(row=2, column=1)
+
+        self.lblDescription = Label(MovieFrame, font=('arial', 12, 'bold'), text='Description', bd=7, anchor='w',
+                                  justify=LEFT)
+        self.lblDescription.grid(row=3, column=0, sticky=W, padx=5, pady=5)
+        self.txtDescription = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
+                                textvariable=self.Description)
+        self.txtDescription.grid(row=3, column=1)
+
+        self.lblStarring = Label(MovieFrame, font=('arial', 12, 'bold'), text='Starring', bd=7, anchor='w',
+                                    justify=LEFT)
+        self.lblStarring.grid(row=4, column=0, sticky=W, padx=5, pady=5)
+        self.txtStarring = Entry(MovieFrame, font=('arial', 12, 'bold'), bd=5, justify='left', width=23,
+                                    textvariable=self.Starring)
+        self.txtStarring.grid(row=4, column=1)
+
+        self.lblGenres = Label(MovieFrame, font=('arial', 12, 'bold'), text='Genres', bd=7, anchor='w',
+                                 justify=LEFT)
+        self.lblGenres.grid(row=5, column=0, sticky=W, padx=5, pady=5)
+        self.cboGenres = ttk.Combobox(MovieFrame, textvariable=self.Genres, width=21,
+                                      font=('arial', 12, 'bold'), state='readonly')
+        self.cboGenres['value'] = ('', 'Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary',
+                                   'Drama', 'Fantasy', 'History', 'Horror', 'Mystery', 'Romance', 'Science Fiction',
+                                   'Thriller', 'War', 'Western')
+        self.cboGenres.current(0)
+        self.cboGenres.grid(row=5, column=1)
+        # ---------------------------------------------------------------------------------------------
+        self.lblAudio = Label(LanguageFrame, font=('arial', 12, 'bold'), text='Audio languages', bd=7, anchor='w',
+                              justify=LEFT)
+        self.lblAudio.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+        self.cboAudio = ttk.Combobox(LanguageFrame, textvariable=self.AudioLanguages, width=21,
+                                      font=('arial', 12, 'bold'), state='readonly')
+        self.cboAudio['value'] = ('', 'English', 'Español (España)', 'Italiano', 'Español (Latinoamérica)', 'Polski',
+                                  'Čeština', '日本語', 'Português', 'Türkçe', 'Français')
+        self.cboAudio.current(0)
+        self.cboAudio.grid(row=0, column=1)
+
+        self.lblSubtitles = Label(LanguageFrame, font=('arial', 12, 'bold'), text='Subtitles', bd=7, anchor='w',
+                                  justify=LEFT)
+        self.lblSubtitles.grid(row=1, column=0, sticky=W, padx=5, pady=5)
+        self.cboSubtitles = ttk.Combobox(LanguageFrame, textvariable=self.Subtitles, width=21,
+                                     font=('arial', 12, 'bold'), state='readonly')
+        if self.AreSubtitles == 'No':
+            self.cboSubtitles['value'] = ('')
+        else:
+            self.cboSubtitles['value'] = ('', 'English', 'Español (España)', 'Italiano', 'Español (Latinoamérica)', 'Polski',
+                                      'Čeština', '日本語', 'Português', 'Türkçe', 'Français')
+        self.cboSubtitles.current(0)
+        self.cboSubtitles.grid(row=1, column=1)
 
 
 if __name__ == '__main__':
